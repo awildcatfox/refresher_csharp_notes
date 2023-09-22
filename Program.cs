f3d0fc49c564901539d29083be11c249f4541749
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2023-09-21 19:33:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace refresher_csharp_notes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
